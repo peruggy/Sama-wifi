3d89aadf3e45c411cc04677c39276a3b2deabe73
omar <topesa@gmail.com>
2020-12-23 03:38:24 -0700

    [ADD] itatix_base_pedimento : se agrega modulo de pedimento

---
+++ itatix_base_pedimento/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Modulo base para propagacion de pedimento agrupador",
+    "author": "ITATIX SA DE CV, ",
+    "version": "14.0.0.0.0",
+    "category": "Inventory",
+    "depends": ["stock","stock_landed_costs"],
+    "data": [
+        "views/stock_picking_view.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
