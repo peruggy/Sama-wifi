2eee3412a21cc9004efe6d6416314e6336ddafa5
omar <topesa@gmail.com>
2021-02-11 05:39:47 -0700

    [REF] se mejora apartado de billing goal

--- itatix_billing_goal/models/sale_order.py
+++ itatix_billing_goal/models/sale_order.py
@@ -1,8 +1,7 @@
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     billing_goal = fields.Float(related='user_id.billing_goal', group_operator="avg", store=True)
-    invoiced_target = fields.Float(related='team_id.invoiced_target', group_operator="avg", store=True)
