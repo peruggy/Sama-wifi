6e4b715b86122e1793ed29f9f2f9c8e4c525e55f
omar <topesa@gmail.com>
2021-02-11 05:49:01 -0700

    Revert "[REF] se mejora apartado de billing goal"
    This reverts commit 2eee3412a21cc9004efe6d6416314e6336ddafa5.

--- itatix_billing_goal/models/sale_order.py
+++ itatix_billing_goal/models/sale_order.py
@@ -1,7 +1,8 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     billing_goal = fields.Float(related='user_id.billing_goal', group_operator="avg", store=True)
+    invoiced_target = fields.Float(related='team_id.invoiced_target', group_operator="avg", store=True)
